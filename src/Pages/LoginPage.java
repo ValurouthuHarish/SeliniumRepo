5895e47f7feace0ace8df8e1333bfc04dfa7f8d3
harish <harish.valurouthu@gmail.com>
2019-03-01 17:27:47 +0530

    added the another and deleted some file

--- src/Pages/LoginPage.java
+++ src/Pages/LoginPage.java
@@ -4,6 +4,5 @@ public class LoginPage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Login Page");
-	}
-
+		System.out.println("How are you");
 }
