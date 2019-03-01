4adbfac3231acabc0fc88e081003f7aec982b87c
harish <harish.valurouthu@gmail.com>
2019-03-01 17:10:40 +0530

    first git commit for my selenium code

---
+++ src/util/utilclass.java
@@ -0,0 +1,10 @@
+package util;
+
+public class utilclass {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Util class Page");
+	}
+
+
+}
---
+++ testing/src/main/java/stepDefinitions/LoginStepDefinition.java
@@ -0,0 +1,26 @@
+package stepDefinitions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import cucumber.api.java.en.Given;
+//import cucumber.api.java.en.Then;
+//import cucumber.api.java.en.When;
+//import junit.framework.Assert;
+
+public class LoginStepDefinition {
+
+	
+	public void user_already_on_login_page() {
+		
+	}
+	
+	
+	
+	
+}
+	
